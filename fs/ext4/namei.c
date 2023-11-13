1a4eda02e4d3cb5ae85f99512db920e686ba1d0b
NotNoelChannel <notnoel12345678@gmail.com>
2023-11-13 17:06:52 +0100

    fs: remove sdcardfs

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1049,20 +1049,11 @@ static inline int search_dirblock(struct buffer_head *bh,
 				  struct inode *dir,
 				  const struct qstr *d_name,
 				  unsigned int offset,
-#ifdef CONFIG_SDCARD_FS_CI_SEARCH
-				  struct ext4_dir_entry_2 ** res_dir,
-				  char *ci_name_buf)
-{
-	return search_dir(bh, bh->b_data, dir->i_sb->s_blocksize, dir,
-			  d_name, offset, res_dir, ci_name_buf);
-}
-#else
 				  struct ext4_dir_entry_2 ** res_dir)
 {
 	return search_dir(bh, bh->b_data, dir->i_sb->s_blocksize, dir,
 			  d_name, offset, res_dir);
 }
-#endif
 
 /*
  * Directory block splitting, compacting
